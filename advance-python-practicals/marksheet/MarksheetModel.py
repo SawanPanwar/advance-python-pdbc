2854b60024339d01444b5757303a398c546faadf
SawanPanwar <skpanwar25@gmail.com>
2024-06-19 11:20:36 +0530

    done

--- advance-python-practicals/marksheet/MarksheetModel.py
+++ advance-python-practicals/marksheet/MarksheetModel.py
@@ -3,8 +3,21 @@ import pymysql
 
 class MarksheetModel:
 
+    def nextPk(self):
+        pk = 0
+        connection = pymysql.connect(host='localhost', port=3306, user='root', password='root', db='adv_python_test')
+        with connection.cursor() as cursor:
+            sql = "select max(id) from marksheet"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            for d in result:
+                if d[0] is not None:
+                    pk = d[0]
+        connection.close()
+        return pk + 1
+
     def add(self, data):
-        id = data["id"]
+        id = MarksheetModel.nextPk(self)
         name = data["name"]
         rollNo = data["rollNo"]
         physics = data["physics"]
@@ -47,11 +60,35 @@ class MarksheetModel:
         connection.close()
         print("Data Deleted...!!!!")
 
-    def read(self):
+    def get(self, id):
         connection = pymysql.connect(host='localhost', port=3306, user='root', password='root', db='adv_python_test')
         with connection.cursor() as cursor:
-            sql = "select * from marksheet"
-            cursor.execute(sql)
+            sql = "select * from marksheet where id = %s"
+            data = [id]
+            cursor.execute(sql, data)
+            result = cursor.fetchall()
+            for data in result:
+                print(data[0], '\t', data[1], '\t', data[2], '\t', data[3], data[4], '\t', data[5])
+        connection.commit()
+        connection.close()
+
+    def search(self, data):
+        name = data.get("name", "")
+        rollNo = data.get("rollNo", 0)
+        pageNo = data.get("pageNo", 0)
+        pageSize = data.get("pageSize", 0)
+        connection = pymysql.connect(host='localhost', port=3306, user='root', password='root', db='adv_python_test')
+        with connection.cursor() as cursor:
+            sql = "select * from marksheet where 1=1"
+            if (name != ""):
+                sql += " and name like '" + name + "%%'"
+            if (rollNo != 0):
+                sql += " and roll_no = " + str(rollNo)
+            if (pageSize > 0):
+                pageNo = (pageNo - 1) * pageSize
+                sql += " limit %s, %s"
+            print("sql => ", sql)
+            cursor.execute(sql, [pageNo, pageSize])
             result = cursor.fetchall()
             for data in result:
                 print(data[0], '\t', data[1], '\t', data[2], '\t', data[3], data[4], '\t', data[5])
--- advance-python-practicals/marksheet/TestMarksheet.py
+++ advance-python-practicals/marksheet/TestMarksheet.py
@@ -3,7 +3,6 @@ from MarksheetModel import MarksheetModel
 
 def testAdd():
     params = {}
-    params["id"] = 1
     params["name"] = "kkk"
     params["rollNo"] = 101
     params["physics"] = 78
@@ -13,27 +12,42 @@ def testAdd():
     model = MarksheetModel()
     model.add(params)
 
+
 def testUpdate():
     params = {}
-    params["id"] = 1
+    params["id"] = 2
     params["name"] = "hhh"
-    params["rollNo"] = 101
-    params["physics"] = 78
-    params["chemistry"] = 67
-    params["maths"] = 89
+    params["rollNo"] = 102
+    params["physics"] = 71
+    params["chemistry"] = 90
+    params["maths"] = 99
 
     model = MarksheetModel()
     model.update(params)
+
 
 def testDelete():
     model = MarksheetModel()
     model.delete(1)
 
-def testRead():
-    model = MarksheetModel()
-    model.read()
 
-#testAdd()
-#testUpdate()
-#testDelete()
-testRead()
+def testSearch():
+    params = {}
+    params["name"] = "k"
+    params["rollNo"] = 101
+    params["pageNo"] = 1
+    params["pageSize"] = 5
+    model = MarksheetModel()
+    model.search(params)
+
+
+def testGet():
+    model = MarksheetModel()
+    model.get(1)
+
+
+# testAdd()
+# testUpdate()
+# testDelete()
+testSearch()
+# testGet()

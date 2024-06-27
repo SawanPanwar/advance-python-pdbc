a18b71bbc5dc955fcdfce14cdec2abe390b5f09d
SawanPanwar <skpanwar25@gmail.com>
2024-06-27 11:14:15 +0530

    done

--- advance-python-practicals/crud/TestDelete.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-connection = pymysql.connect(host='localhost', port=3306, user='root', password='root', db='adv_python_test')
-with connection.cursor() as cursor:
-    sql = "delete from emp where id = 13"
-    cursor.execute(sql)
-connection.commit()
-connection.close()
-print("Data Deleted...!!!!")

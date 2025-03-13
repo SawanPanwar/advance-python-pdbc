6c60d3fd8ea3b8ea8be2fceb26ccd1a2349370d3
SawanPanwar <skpanwar25@gmail.com>
2025-03-13 11:57:27 +0530

    13-03-2025

--- advance-python-practicals/crud/TestInsert.py
+++ advance-python-practicals/crud/TestInsert.py
@@ -2,7 +2,7 @@ import pymysql
 
 connection = pymysql.connect(host='localhost', port=3306, user='root', password='root', db='advance_python')
 cursor = connection.cursor()
-sql = "insert into marksheet values(2, 102, 'raj', 13, 48, 38)"
+sql = "insert into marksheet values(10, 110, 'raj', 13, 48, 38)"
 cursor.execute(sql)
 connection.commit()
 connection.close()
